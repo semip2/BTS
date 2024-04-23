76377df35c9c6bc32b43589e935051c4ce41f6e4
Samuel Lim <samplim@umich.edu>
2024-04-23 18:12:40 -0400

    added space labels

--- tts.py
+++ tts.py
@@ -1,16 +1,5 @@
 from gtts import gTTS
 
-mytext = 'Welcome to geeksforgeeks!'
-
-# Language in which you want to convert
-language = 'en'
-
-# Passing the text and language to the engine, 
-# here we have marked slow=False. Which tells 
-# the module that the converted audio should 
-# have a high speed
-myobj = gTTS(text=mytext, lang=language, slow=False)
-
-# Saving the converted audio in a mp3 file named
-# welcome 
-myobj.save("welcome.mp3")
+def transcribe(input, filename, language='en'):
+    audio = gTTS(text=input, lang=language, slow=False)
+    audio.save(f"{filename}.mp3")
